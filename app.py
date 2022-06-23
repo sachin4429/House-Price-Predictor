561a319c22f7bff4661eb0da1f58d02b82b97a06
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-06-23 20:49:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,18 +13,6 @@ pipe = pickle.load(open('RidgeModel.pkl', 'rb'))
 def home():
     locations = sorted(data['location'].unique())
     return render_template('index.html',locations=locations)
-
-@app.route('/predict',methods=['POST'])
-@cross_origin()
-def predict():
-    location = request.form.get('location')
-    bhk = float(request.form.get('bhk'))
-    bath = float(request.form.get('bath'))
-    sqft = request.form.get('total_sqft')
-
-    print( location, bhk, bath ,sqft)
-    
-    return "Hellow World"
     
 if __name__ == "__main__":
     app.run(debug=True)
