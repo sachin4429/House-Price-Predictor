4f2408a68628b3b6e1bf87893c6ca73a45590049
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-06-23 14:56:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import pandas as pd
+from flask_cors import cross_origin
+
+app = Flask(__name__)
+data = pd.read_csv('Cleaned_data.csv')
+pipe = pickle.load(open('RidgeModel.pkl', 'rb'))
+
+@app.route("/")
+@cross_origin()
+def home():
+    locations = sorted(data['location'].unique())
+    return render_template('index.html',locations=locations)
+
+@app.route('/predict',methods=['POST'])
+@cross_origin()
+def predict():
+    location = request.form.get('location')
+    bhk = float(request.form.get('bhk'))
+    bath = float(request.form.get('bath'))
+    sqft = request.form.get('total_sqft')
+
+    print( location, bhk, bath ,sqft)
+    
+    input = pd.DataFrame([[location, sqft, bath, bhk]], columns = ['location', 'total_sqft', 'bath' ,'bhk'])
+    prediction = pipe.predict(input)[0] * 1e5
+    return str(np.round(prediction,2))
+    
+if __name__ == "__main__":
+    app.run(debug=True)
